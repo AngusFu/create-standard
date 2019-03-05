012c7acf1fd69d5be146b9e1d2cff1ed5bd8947a
wemlion <angusfu1126@qq.com>
2019-03-06 00:46:28 +0800

    fix: error with pkg-install

--- main.js
+++ main.js
@@ -102,7 +102,7 @@ const extendPkgJson = function() {
   writeFileSync(pkgPath, JSON.stringify(pkgJSON, null, 2))
 }
 
-getPackageManager()
+getPackageManager({ cwd: process.cwd() })
   .then(pkgManager =>
     Promise.resolve()
       .then(() => console.log(''))
