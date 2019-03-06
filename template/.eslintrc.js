8d35714ebcc077fd9d7d6e0ca2403136a4761f7c
wemlion <angusfu1126@qq.com>
2019-03-06 23:03:59 +0800

    fix: eslint env

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
   env: {
     es6: true,
     node: true,
-    jest: true
+    jest: true,
+    browser: true
   },
   plugins: ['prettier'],
   extends: ['eslint:recommended', require.resolve('eslint-config-prettier')],
